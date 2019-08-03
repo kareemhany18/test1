aa03ec2a5c53cff49624f0b6b88f6a5f006c0792
kareemhany18 <53636799+kareemhany18@users.noreply.github.com>
2019-08-03 08:57:14 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("607103894853517324")
+setInterval(function() {
+channel.send(`تـآآيـيـلر كاريزما تـآآيـيـلر كاريزما تـآآيـيـلر كاريزما تـآآيـيـلر كاريزما تـآآيـيـلر كاريزما تـآآيـيـلر كاريزما تـآآيـيـلر كاريزما`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
